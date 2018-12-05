0cc49f4b3deb37f4ec151c928839bade78d62e57
Pannapur <pannapur@in-Pannapur.local>
2018-12-05 14:55:58 +0530

    Integrated library into main file.

--- head.js
+++ head.js
@@ -1,18 +1,8 @@
-/* 
-  Usage:
-  node ./head.js file1
-  node ./head.js -n5 file1
-  node ./head.js -n 5 file1
-  node ./head.js -5 file1
-  node ./head.js file1 file2
-  node ./head.js -n 5 file1 file2
-  node ./head.js -n5 file1 file2
-  node ./head.js -5 file1 file2 
-  node ./head.js -c5 file1
-  node ./head.js -c 5 file1
-  node ./head.js -c5 file1 file2
-  node ./head.js -c 5 file1 file2
-*/
+const readContent = require("fs").readFileSync;
+const { head } = require("./src/lib.js");
 
+const main = function(args) {
+  console.log(head(args,readContent));
+}
 
-
+main(process.argv)
