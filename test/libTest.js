04541ba4838ed4b471f1ade6a32eef40a7e98849
Pannapur <pannapur@in-Pannapur.local>
2018-12-06 17:18:50 +0530

    Modified output to exectedOutput.

--- test/libTest.js
+++ test/libTest.js
@@ -8,50 +8,50 @@ const { extractLines,
 
 describe('extractLines', function() {
   const file = ["There are 5 types of lines:","Horizontal line.","Vertical line.","Skew Lines.","Parallel Lines.","Perpendicular Lines."]
-  let output;
+  let expectedOutput;
 
   it('should return empty string when file is empty', function() {
     deepEqual(extractLines([],2),'');
   });
 
   it('should return single line from file when number of line is 1', function() {
-    output = "There are 5 types of lines:"; 
-    deepEqual(extractLines(file,1),output); 
+    expectedOutput = "There are 5 types of lines:"; 
+    deepEqual(extractLines(file,1),expectedOutput); 
   });
 
   it('should return specified number of lines joined with \\n ', function() {
-    output = "There are 5 types of lines:\nHorizontal line."; 
-    deepEqual(extractLines(file,2),output);
+    expectedOutput = "There are 5 types of lines:\nHorizontal line."; 
+    deepEqual(extractLines(file,2),expectedOutput);
 
-    output = "There are 5 types of lines:\nHorizontal line.\nVertical line."; 
-    deepEqual(extractLines(file,3),output);
+    expectedOutput = "There are 5 types of lines:\nHorizontal line.\nVertical line."; 
+    deepEqual(extractLines(file,3),expectedOutput);
   });
 
   it('should return whole file if length is not specified', function() {
-    output = "There are 5 types of lines:\nHorizontal line.\nVertical line.\nSkew Lines.\nParallel Lines.\nPerpendicular Lines."
-    deepEqual(extractLines(file),output); 
+    expectedOutput = "There are 5 types of lines:\nHorizontal line.\nVertical line.\nSkew Lines.\nParallel Lines.\nPerpendicular Lines."
+    deepEqual(extractLines(file),expectedOutput); 
   });
 });
 
 describe('extractCharacters', function() {
   const file = ["There are 5 types of lines:","Horizontal line.","Vertical line.","Skew Lines.","Parallel Lines.","Perpendicular Lines."]
-  let output;
+  let expectedOutput;
 
   it('should return empty string when file is empty', function() {
     deepEqual(extractCharacters([],2),'');
   });
 
   it('should return specified number of characters from file', function() {
-    output = "T";
-    deepEqual(extractCharacters(file,1),output);
+    expectedOutput = "T";
+    deepEqual(extractCharacters(file,1),expectedOutput);
 
-    output = "There ";
-    deepEqual(extractCharacters(file,6),output);
+    expectedOutput = "There ";
+    deepEqual(extractCharacters(file,6),expectedOutput);
   });
 
   it('should return whole file if number of characters is not specified', function() {
-    output = "There are 5 types of lines:\nHorizontal line.\nVertical line.\nSkew Lines.\nParallel Lines.\nPerpendicular Lines."
-    deepEqual(extractCharacters(file),output);
+    expectedOutput = "There are 5 types of lines:\nHorizontal line.\nVertical line.\nSkew Lines.\nParallel Lines.\nPerpendicular Lines."
+    deepEqual(extractCharacters(file),expectedOutput);
   });
 });
 
