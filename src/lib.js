6c6636601c7d8ad4c78124f5fe01ca10167af123
Pannapur <pannapur@in-Pannapur.local>
2018-12-06 13:04:51 +0530

    Removed unnecessary spaces.

--- src/lib.js
+++ src/lib.js
@@ -1,9 +1,6 @@
 const errorMessage = 'head: illegal option -- ';
-
 const usageMessage = 'usage: head [-n lines | -c bytes] [file ...]';
-
 const invalidLineCount = 'head: illegal line count -- ';
-
 const invalidByteCount = 'head: illegal byte count -- ';
 
 const extractLines = function(file, numberOfLines) {
@@ -16,19 +13,15 @@ const extractCharacters = function(file, numberOfCharacters) {
 
 const organizeInput = function(args) {
   let organizedInput = {option:"n", count:10, files:args.slice(2)};
-
   if(args[2]=="-c" || args[2]=="-n") {
     organizedInput = {option : args[2][1], count : parseInt(args[3]), files : args.slice(4)};
   }
-
   if(args[2].length>2 && args[2].includes('-')){
     organizedInput = { option : args[2].slice(1,2), count : args[2].slice(2), files : args.slice(3)};
   }
-
   if(parseInt(args[2])){
     organizedInput = { option : "n", count : Math.abs(args[2]), files : args.slice(3)};
   }
-
   return organizedInput;
 }
 
@@ -54,7 +47,6 @@ const getContent = function(fileDetails, validater, readContent) {
     return funcRef(readContent(files[0],'utf8').split('\n'),count);
   }
   return files.reduce(fetchData, details).output.join("\n");
-
 }
 
 const head = function(fileDetails,validater,readContent){
@@ -62,11 +54,9 @@ const head = function(fileDetails,validater,readContent){
   if(fileDetails[2]== 0 || count==0){
     return invalidLineCount + "0";
   }
-
   if (isNaN(count - 0) || count < 1) {
     return (option == 'n') ? invalidLineCount + count : invalidByteCount + count;
   } 
-
   if (fileDetails[2][0]=='-' && fileDetails[2][1] != 'c' && fileDetails[2][1] != 'n' && !parseInt(fileDetails[2])) {
     return errorMessage + fileDetails[2][1] + '\n' + usageMessage;
   }
