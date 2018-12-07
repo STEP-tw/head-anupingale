262f0ade23b050e417545dff5b14188502e296e0
Pannapur <pannapur@in-Pannapur.local>
2018-12-07 17:37:11 +0530

    Modified head input arguments.

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
-const {readFileSync, existsSync} = require("fs");
+const fs = require("fs");
 const { head } = require("./src/lib.js");
 
 const main = function(args) {
-  console.log(head(args.slice(2), existsSync, readFileSync));
+  console.log(head(args.slice(2), fs));
 }
 
 main(process.argv)
