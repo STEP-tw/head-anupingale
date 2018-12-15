ff8835f533e7e4a2910a6614bd6b62e2ef82a670
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2018-12-15 06:27:17 +0530

    Intosuced functionality to check whether the count is valid or not.

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -1,55 +1,62 @@
-const {hasDash} = require("./parser.js");
+const { hasDash } = require("./parser.js");
 
-const isZero = function(value) {
-    return value == 0;
+const isZero = function (value) {
+  return value == 0;
 };
 
-const invalidCount = function(option, count) {
-    return option == "n"
-      ? "head: illegal line count -- " + count
-      : "head: illegal byte count -- " + count;
-  };
+const hasInvalidCount = function (count) {
+  return isNaN(count) || count < 1;
+}
 
-const hasInvalidOption = function(fileDetails) {
-    let invalidOption = hasDash(fileDetails[0][0]) &&
-    !["n","c"].includes(fileDetails[0][1]);
-    return invalidOption && !parseInt(fileDetails[0]);
-  };
+const invalidCount = function (option, count) {
+  let errors = {
+    n: "head: illegal line count -- ",
+    c: "head: illegal byte count -- "
+  }
+  return errors[option] + count;
+};
 
-const validateHeadArguments = function(fileDetails, count, option) {
-    if (isZero(fileDetails[0])) {
-      return "head: illegal line count -- 0";
-    }
-    if (isNaN(count - 0) || count < 1) {
-      return invalidCount(option, count);
-    }
-    if (hasInvalidOption(fileDetails)) {
-      return (
-        "head: illegal option -- " + fileDetails[0][1] + "\n" + "usage: head [-n lines | -c bytes] [file ...]"
-      );
-    }
+const hasInvalidOption = function (fileDetails) {
+  let invalidOption = hasDash(fileDetails[0]) &&
+    !["n", "c"].includes(fileDetails[1]);
+  return invalidOption && !parseInt(fileDetails);
+};
+
+const validateHeadArguments = function (fileDetails, count, option) {
+  if (isZero(fileDetails[0])) {
+    return "head: illegal line count -- 0";
+  }
+  if (hasInvalidCount(count)) {
+    return invalidCount(option, count);
+  }
+  if (hasInvalidOption(fileDetails[0])) {
+    return (
+      "head: illegal option -- " + fileDetails[0][1] + "\n" + "usage: head [-n lines | -c bytes] [file ...]"
+    );
+  }
+}
+
+const validateTailArguments = function (fileDetails, count, files) {
+  if (files.includes("-0") || count == 0) {
+    return "";
   }
 
-  const validateTailArguments = function(fileDetails, count, files) {
-    if (files.includes("-0") || count == 0) {
-      return "";
-    }
-  
-    if (isNaN(count)) {
-      return "tail: illegal offset -- " + fileDetails[0].slice(2);
-    }
-  
-    if (hasInvalidOption(fileDetails)) {
-      let error = "tail: illegal option -- " + fileDetails[0][1] + "\n";
-      error += "usage: tail [-n lines | -c bytes] [file ...]";
-      return error;
-    }
+  if (isNaN(count)) {
+    return "tail: illegal offset -- " + fileDetails[0].slice(2);
   }
 
-  module.exports = {
-    validateHeadArguments, 
-    validateTailArguments, 
-    isZero, 
-    hasInvalidOption, 
-    invalidCount
-  };
+  if (hasInvalidOption(fileDetails[0])) {
+    let error = "tail: illegal option -- " + fileDetails[0][1] + "\n";
+    error += "usage: tail [-n lines | -c bytes] [file ...]";
+    return error;
+  }
+}
+
+module.exports = {
+  validateHeadArguments,
+  validateTailArguments,
+  isZero,
+  hasInvalidOption,
+  invalidCount,
+  hasInvalidCount
+};
--- test/errorHandlerTest.js
+++ test/errorHandlerTest.js
@@ -1,64 +1,80 @@
 const { deepEqual } = require("assert");
-const {validateHeadArguments, validateTailArguments, isZero, hasInvalidOption, invalidCount} = require("../src/errorHandler.js");
+const {
+  validateHeadArguments,
+  validateTailArguments,
+  isZero,
+  hasInvalidOption,
+  invalidCount,
+  hasInvalidCount } = require("../src/errorHandler.js");
 
-describe('validateHeadArguments', function() {
-    it('should return undefined if function condition is false', function() {
-      deepEqual(validateHeadArguments(["n", "10","numbers"], 10, "n"), undefined);
-    });
-  
-    it('should return error if count is invalid', function() {
-      expectedOutput = "head: illegal line count -- 0";
-      deepEqual(validateHeadArguments(["n","-0","head"], 0, "n"), expectedOutput);
-    });
-  
-    it('should return error filename is invalid', function() {
-        expectedOutput = "head: illegal option -- z\nusage: head [-n lines | -c bytes] [file ...]";
-        deepEqual(validateHeadArguments(["-z","10","abc"], 10, "z"), expectedOutput);
-    });
-  }); 
-
-  describe("isZero", function() {
-    it("should return true if the value is zero", function() {
-      deepEqual(isZero(0), true);
-    });
-  
-    it("should return false if the value is nonZero", function() {
-      deepEqual(isZero(2), false);
-    });
+describe('validateHeadArguments', function () {
+  it('should return undefined if function condition is false', function () {
+    deepEqual(validateHeadArguments(["n", "10", "numbers"], 10, "n"), undefined);
   });
 
-  describe("invalidCount", function() {
-    it("should return invalid line count if option is n", function() {
-      expectedOutput = "head: illegal line count -- 5";
-      deepEqual(invalidCount("n", 5), expectedOutput);
-    });
-  
-    it("should return invalid byte count if option is c", function() {
-      expectedOutput = "head: illegal byte count -- 5";
-      deepEqual(invalidCount("c", 5), expectedOutput);
-    });
+  it('should return error if count is invalid', function () {
+    expectedOutput = "head: illegal line count -- 0";
+    deepEqual(validateHeadArguments(["n", "-0", "head"], 0, "n"), expectedOutput);
   });
-  describe("hasInvalidOption", function() {
-    it("should return true if has chracters except options", function() {
-      deepEqual(hasInvalidOption(["-z"]), true);
-    });
-  
-    it("should return false if has option", function() {
-      deepEqual(hasInvalidOption(["-n"]), false);
-    });
-  });  
 
-  describe('validateTailArguments', function() {
-    it('should return undefined if function condition is false', function() {
-      deepEqual(validateTailArguments(["n", "10","numbers"], 10, "n"), undefined);
-    });
-  
-    it('should return error if count is invalid', function() {
-      deepEqual(validateTailArguments(["n","-0","tail"], 0, "n"), "");
-    });
-  
-    it('should return error filename is invalid', function() {
-        expectedOutput = "tail: illegal option -- z\nusage: tail [-n lines | -c bytes] [file ...]";
-        deepEqual(validateTailArguments(["-z","10","abc"], 10, "z"), expectedOutput);
-    });
+  it('should return error filename is invalid', function () {
+    expectedOutput = "head: illegal option -- z\nusage: head [-n lines | -c bytes] [file ...]";
+    deepEqual(validateHeadArguments(["-z", "10", "abc"], 10, "z"), expectedOutput);
   });
+});
+
+describe("isZero", function () {
+  it("should return true if the value is zero", function () {
+    deepEqual(isZero(0), true);
+  });
+
+  it("should return false if the value is nonZero", function () {
+    deepEqual(isZero(2), false);
+  });
+});
+
+describe("invalidCount", function () {
+  it("should return invalid line count if option is n", function () {
+    expectedOutput = "head: illegal line count -- 5";
+    deepEqual(invalidCount("n", 5), expectedOutput);
+  });
+
+  it("should return invalid byte count if option is c", function () {
+    expectedOutput = "head: illegal byte count -- 5";
+    deepEqual(invalidCount("c", 5), expectedOutput);
+  });
+});
+describe("hasInvalidOption", function () {
+  it("should return true if has chracters except options", function () {
+    deepEqual(hasInvalidOption(["-z"]), true);
+  });
+
+  it("should return false if has option", function () {
+    deepEqual(hasInvalidOption(["n"]), false);
+  });
+});
+
+describe('validateTailArguments', function () {
+  it('should return undefined if function condition is false', function () {
+    deepEqual(validateTailArguments(["n", "10", "numbers"], 10, "n"), undefined);
+  });
+
+  it('should return error if count is invalid', function () {
+    deepEqual(validateTailArguments(["n", "-0", "tail"], 0, "n"), "");
+  });
+
+  it('should return error filename is invalid', function () {
+    expectedOutput = "tail: illegal option -- z\nusage: tail [-n lines | -c bytes] [file ...]";
+    deepEqual(validateTailArguments(["-z", "10", "abc"], 10, "z"), expectedOutput);
+  });
+});
+
+describe('hasInvalidCount', function () {
+  it('should return true if count is not a number or less than 1', function () {
+    deepEqual(hasInvalidCount("10u"), true);
+  });
+
+  it('should return false if count is a number number and greater than 1 ', function () {
+    deepEqual(hasInvalidCount(5), false);
+  });
+});
