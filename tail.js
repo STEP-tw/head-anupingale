e14b3ce2117f65c3e99d57135499832f304a7f51
Annapurna Pingale <44020027+anupingale@users.noreply.github.com>
2018-12-21 10:07:13 +0530

    Used tail function in tail.js.

--- tail.js
+++ tail.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const { parse } = require('./src/parser.js');
-const { getContent } = require('./src/lib.js');
+const { tail } = require('./src/lib.js');
 
 const main = function(args) {
 	let parameters = parse(args.slice(2));
-	console.log(getContent(parameters, fs));
+	console.log(tail(parameters, fs));
 };
 
 main(process.argv);
